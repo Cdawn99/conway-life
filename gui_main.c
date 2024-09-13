ab994b28a38115f8144850e939f7ac1cd7ff832b
Cdawn99 <140974895+Cdawn99@users.noreply.github.com>
2024-09-13 14:17:34 +0200

    Add ability to speed up/slow down to GUI version

--- gui_main.c
+++ gui_main.c
@@ -7,6 +7,10 @@
 #define GRID_HEIGHT_CELLS 40
 #define GRID_WIDTH_CELLS 71
 
+#define MAXIMUM_MAX_FRAME_DELAY 20
+#define MINIMUM_MAX_FRAME_DELAY 1
+#define DEFAULT_MAX_FRAME_DELAY 10
+
 void display_grid(grid_t *grid) {
     Rectangle cell = {
         .width = (float)GetScreenWidth() / grid->width,
@@ -69,10 +73,12 @@ int main(void) {
     SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
 
-    int frame_delay = 10;
+    int max_frame_delay = DEFAULT_MAX_FRAME_DELAY;
+    int frame_delay = max_frame_delay;
     bool playing = true;
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_R)) {
+            max_frame_delay = DEFAULT_MAX_FRAME_DELAY;
             status = reset_state(&current, &next);
             if (status == 1) {
                 break;
@@ -83,9 +89,22 @@ int main(void) {
             playing = !playing;
         }
 
+        if (IsKeyDown(KEY_RIGHT) && max_frame_delay > MINIMUM_MAX_FRAME_DELAY) {
+            max_frame_delay--;
+        }
+
+        if (IsKeyDown(KEY_LEFT) && max_frame_delay < MAXIMUM_MAX_FRAME_DELAY) {
+            max_frame_delay++;
+        }
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
             display_grid(current);
+
+            // char frame_delay_text[30] = {0};
+            // sprintf(frame_delay_text, "Frame delay: %d", max_frame_delay);
+            // DrawText(frame_delay_text, 10, 10, 20, RED);
+
             // DrawFPS(10, 10);
         EndDrawing();
 
@@ -95,7 +114,7 @@ int main(void) {
             } else {
                 compute_next_state(current, next);
                 swap_grids(&current, &next);
-                frame_delay = 10;
+                frame_delay = max_frame_delay;
             }
         }
     }
